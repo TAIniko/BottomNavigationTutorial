e4c02f8bb9ae695e6271d2c2ed31053233f25a50
TAiniko <taisuke.sum@gmail.com>
2019-12-24 09:41:13 +0900

    Initial commit

---
+++ app/src/main/java/com/tainiko/bottomnavigationtutorial/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.tainiko.bottomnavigationtutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    //create three object for fragment as create in java
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var favoritesFragment: FavoritesFragment
+    lateinit var profileFragment: ProfileFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.btm_nav)
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+        bottomNavigation.setOnNavigationItemSelectedListener {item ->
+            when(item.itemId){
+
+                R.id.home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.favorites -> {
+                    favoritesFragment = FavoritesFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, favoritesFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.profile -> {
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+            }
+
+            true
+        }
+
+    }
+}
